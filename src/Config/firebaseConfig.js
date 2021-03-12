d41d97c3e1dd51ee9a6512dfba22515ae1d56279
Anuj-Negi <negianuj27@gmail.com>
2021-03-12 15:16:25 +0530

    Initial commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyD2hftoJuhmdezkXos8FXsr90eFEzlE9kM",
+    authDomain: "mygitapp-d9f6d.firebaseapp.com",
+    projectId: "mygitapp-d9f6d",
+    storageBucket: "mygitapp-d9f6d.appspot.com",
+    messagingSenderId: "183610805766",
+    appId: "1:183610805766:web:eac5d569da028575fa3d87",
+    measurementId: "G-RTE19WPRT7"
+  };
